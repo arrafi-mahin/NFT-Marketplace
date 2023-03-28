e79e8acfb283de8a63a30c0e12afbe2d8db9802e
Ar Rafi Uddin <arrafi.mahin@gmail.com>
2023-03-28 23:12:40 +0600

    collection section done

--- nft/src/Components/Collection/Collection.js
+++ nft/src/Components/Collection/Collection.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import CollectionCard from "../UI Elements/CollectionCard/CollectionCard";
 import Display from "../UI Elements/CollectionDesplay/Display";
+import TopCard from "../UI Elements/TopCollectionCard/TopCard";
 import styles from "./Collection.module.css";
 function Collection(props) {
   return (
@@ -10,18 +11,26 @@ function Collection(props) {
         <Row>
           <Col md={8} sm={12} className="">
             <Row>
-              <Col md={6} sm={12}>
+              <Col md={6} sm={12} className="mb-4">
                 <Display />
               </Col>
-              <Col md={6} sm={12}>
+              <Col md={6} sm={12} className={styles.collectionCard}>
                 <CollectionCard />
                 <CollectionCard />
                 <CollectionCard />
               </Col>
             </Row>
           </Col>
-          <Col md={4} sm={12}>
-            2
+          <Col md={4} sm={12} className={styles.recent}>
+            <h2 className={styles.title}>Top Collections over</h2>
+            <p className={styles.duration}>Last 7 days</p>
+            <div>
+              <TopCard verified={true} />
+              <TopCard />
+              <TopCard />
+              <TopCard verified={true} />
+              <TopCard />
+            </div>
           </Col>
         </Row>
       </Container>
--- nft/src/Components/UI Elements/CollectionDesplay/Display.js
+++ nft/src/Components/UI Elements/CollectionDesplay/Display.js
@@ -8,12 +8,42 @@ function Display(props) {
     <div className={styles.display}>
       <img className={styles.img} src={bg1} alt="cardImage" />
       <div className={styles.info}>
-        <div>
+        <div className={styles.userInfo}>
           <img src={user} alt="user" />
           <div>
-            <p>The Futr Abstr</p>
-            <p>10 in the stock</p>
+            <p className={styles.title}>The Futr Abstr</p>
+            <p className={styles.stock}>10 in the stock</p>
           </div>
+        </div>
+        <div className={styles.bid}>
+          <p>Highest Bid</p>
+          <span>
+            <svg
+              width="13"
+              height="22"
+              viewBox="0 0 13 22"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g clip-path="url(#clip0_0_213)">
+                <path
+                  d="M12.671 11.4564L6.50004 15.3231L0.325043 11.4564L6.50004 0.589722L12.671 11.4564ZM6.50004 16.5647L0.325043 12.6981L6.50004 21.9231L12.675 12.6981L6.50004 16.5647Z"
+                  fill="#3A3A3A"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0_0_213">
+                  <rect
+                    width="13"
+                    height="21.3333"
+                    fill="white"
+                    transform="translate(0 0.589722)"
+                  />
+                </clipPath>
+              </defs>
+            </svg>
+            0.25ETH
+          </span>
         </div>
       </div>
     </div>
