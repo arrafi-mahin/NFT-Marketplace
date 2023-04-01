5ee869c952be6b14f82fb6b92c7e15de881f55c1
arrafi-mahin <48198243+arrafi-mahin@users.noreply.github.com>
2023-04-01 18:18:53 +0600

    ..

--- src/Components/UI Elements/FeatureDisplay/Display.js
+++ src/Components/UI Elements/FeatureDisplay/Display.js
@@ -1,25 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button/Button";
 import styles from "./Display.module.css";
 import bg1 from "../../../Assets/Image/bg1.png";
 import user from "../../../Assets/Image/user.png";
 function Display(props) {
+  const [collection, setCollection] = useState([
+    {
+      id: 0,
+      name: "Arkhan",
+      title: "Amezing Collection",
+      user: "https://source.unsplash.com/random/900×700/?face",
+      stack: 10,
+      item: [
+        "https://source.unsplash.com/random/300×301/?art",
+        "https://source.unsplash.com/random/300×300/?art",
+        "https://source.unsplash.com/random/300×302/?art",
+      ],
+    },
+  ]);
+  const [display, setDisplay] = useState(collection[0]);
+  const clickHandler = (id) => {};
   return (
-    <div className={styles.card}>
+    <div key={display.id} className={styles.card}>
       <div className={styles.display}>
-        <img src={bg1} alt="Display" />
+        <img src={display.item[0]} alt="Display" />
+
         <div className={styles.sideBar}>
-          <img src={bg1} alt="Display" />
-          <img src={bg1} alt="Display" />
-          <img src={bg1} alt="Display" />
+          {display.item.map((item, index) => {
+            return (
+              <img
+                onClick={clickHandler}
+                key={index}
+                src={item}
+                alt="Display"
+              />
+            );
+          })}
         </div>
       </div>
       <div className={styles.info}>
         <div>
           <h5>Amazing Collection</h5>
           <div>
-            <img src={user} alt="User" />
-            <span>by Arkhan</span>
+            <img src={display.user} alt="User" />
+            <span>{display.name}</span>
           </div>
         </div>
         <Button
