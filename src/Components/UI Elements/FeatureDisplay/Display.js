87462a5cc5e2ab9dca6e6490bc8e789247781546
arrafi-mahin <48198243+arrafi-mahin@users.noreply.github.com>
2023-04-02 11:20:54 +0600

    ..

--- src/Components/UI Elements/FeatureDisplay/Display.js
+++ src/Components/UI Elements/FeatureDisplay/Display.js
@@ -4,32 +4,36 @@ import styles from "./Display.module.css";
 import bg1 from "../../../Assets/Image/bg1.png";
 import user from "../../../Assets/Image/user.png";
 function Display(props) {
-  const [collection, setCollection] = useState([
-    {
-      id: 0,
-      name: "Arkhan",
-      title: "Amezing Collection",
-      user: "https://source.unsplash.com/random/900×700/?face",
-      stack: 10,
-      item: [
-        "https://source.unsplash.com/random/300×301/?art",
-        "https://source.unsplash.com/random/300×300/?art",
-        "https://source.unsplash.com/random/300×302/?art",
-      ],
-    },
-  ]);
-  const [display, setDisplay] = useState(collection[0]);
-  const clickHandler = (id) => {};
+  const [collection, setCollection] = useState({
+    id: 0,
+    name: "Arkhan",
+    title: "Amezing Collection",
+    user: "https://source.unsplash.com/random/900×700/?face",
+    stack: 10,
+    item: [
+      "https://source.unsplash.com/random/300×301/?art",
+      "https://source.unsplash.com/random/300×300/?art",
+      "https://source.unsplash.com/random/300×302/?art",
+    ],
+  });
+  const [display, setDisplay] = useState(collection.item[0]);
+  const clickHandler = (id) => {
+    setDisplay(collection.item[id]);
+    console.log(collection.item[id]);
+  };
   return (
     <div key={display.id} className={styles.card}>
       <div className={styles.display}>
-        <img src={display.item[0]} alt="Display" />
+        <img src={display} alt="Display" />
 
         <div className={styles.sideBar}>
-          {display.item.map((item, index) => {
+          {collection.item.map((item, index) => {
             return (
               <img
-                onClick={clickHandler}
+                onClick={() => {
+                  clickHandler(index);
+                  console.log(index);
+                }}
                 key={index}
                 src={item}
                 alt="Display"
@@ -42,8 +46,8 @@ function Display(props) {
         <div>
           <h5>Amazing Collection</h5>
           <div>
-            <img src={display.user} alt="User" />
-            <span>{display.name}</span>
+            <img src={collection.user} alt="User" />
+            <span>{collection.name}</span>
           </div>
         </div>
         <Button
